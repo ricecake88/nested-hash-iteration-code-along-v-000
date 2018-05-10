accf467c2e3e1f22f9f1ecb365756fcc4659b95b
Grace Shih <vocalise@gmail.com>
2018-05-10 01:18:43 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
    contacts.each do |contact, data|
      data.each do |key, value|
-       if key == :favorite_ice_cream_flavors
-         value.shift
+       if key == :favorite_ice_cream_flavors && value == "strawberry"
        end
      end
     end
